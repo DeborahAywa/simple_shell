9a2c511c44ca6522e95b77d0c72dc50e9f464b83
DeborahAywa <debraokinda@gmail.com>
2023-05-25 03:22:21 -0700

    path search

--- path_search.c
+++ path_search.c
@@ -2,7 +2,6 @@
 /**
  * search_path - searches for the path of a command in environment
  * @cmd: command whose path is searched for
- *
  * Return: full path of command, NULL otherwise
  */
 char *search_path(char *cmd)
@@ -10,31 +9,30 @@ char *search_path(char *cmd)
 	char *path = getenv("PATH");
 	char *duplicate, *path_token, *full_path;
 
-	if(path)
+	if (path)
 	{
 		duplicate = strdup(path);
 		path_token = strtok(duplicate, ":");
-
-		while(path_token != NULL)
+		while (path_token != NULL)
 		{
 			full_path = malloc(BUFFER_SIZE);
 			strcpy(full_path, path_token);
 			strcat(full_path, "/");
 			strcat(full_path, cmd);
 			strcat(full_path, "\0");
-			if(access(full_path, X_OK) == 0)
+			if (access(full_path, X_OK) == 0)
 			{
 				free(duplicate);
-				return(full_path);
+				return (full_path);
 			}
-			else 
+			else
 			{
 				free(full_path);
 				path_token = strtok(NULL, ":");
 			}
 		}
 		free(duplicate);
-		if(access(cmd, X_OK) == 0)
+		if (access(cmd, X_OK) == 0)
 			return (cmd);
 		return (NULL);
 	}
