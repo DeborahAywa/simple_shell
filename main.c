69bf97a190b2a1c78dbe404d197203d0ae486547
DeborahAywa <debraokinda@gmail.com>
2023-07-26 03:47:10 -0700

    editing the string in the function

--- main.c
+++ main.c
@@ -12,7 +12,10 @@ int main(int ac, char **av)
 	command_t command[] = {COMMAND_INIT};
 	int fd = 2;
 
-	asm("mov %1, %0\n\t" "add $ 3, % 0" : " = r" (fd) : "r" (fd));
+	asm("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
